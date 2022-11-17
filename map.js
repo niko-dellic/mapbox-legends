7fe227b7732daf6f6d14ad5ed7732bc3943c70d5
NIKO <39039032+niko-dellic@users.noreply.github.com>
2022-11-16 20:58:15 -0500

    proper data

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
 import sourceInfo from "./data/stateData.geojson" assert { type: "json" };
+import nycRace from "./data/ethnicity.json" assert { type: "json" };
+import nycData from "./data/community_boards.json" assert { type: "json" };
 
 // number of bins for your legend
 const numberOfBins = 6;
@@ -39,15 +41,15 @@ let colorRamp = {
   ],
 };
 
-const selectedColorRamp = colorRamp.blue;
+const selectedColorRamp = colorRamp.red;
 
-const scaleTypes = {
-  quantile: getQuantileScale(),
-  equalInterval: getEqualIntervalScale(),
-  qualitative: getQualitativeScale(),
-};
+// const scaleTypes = {
+//   quantile: getQuantileScale(),
+//   equalInterval: getEqualIntervalScale(),
+//   qualitative: getQualitativeScale(),
+// };
 
-const selectedScale = scaleTypes.quantile;
+// const selectedScale = scaleTypes.quantile;
 
 /**
  * CASE 1: QUANTILE SCALE:
@@ -55,10 +57,15 @@ const selectedScale = scaleTypes.quantile;
  * (i.e. equal number of individuals for each color)
  */
 
-function getQuantileScale() {
-  // get density property from each state info entry
-  const data = sourceInfo.features
-    .map((state) => state.properties.density)
+function getQuantileScale(jsonSource, prop) {
+  /**
+   * @param {array} jsonSource - the data source
+   * @param {string} prop - the property to be used for the scale
+   */
+
+  console.log(jsonSource, prop);
+  const data = jsonSource.features
+    .map((state) => state.properties[prop])
     .sort((a, b) => a - b);
 
   const color = d3.scaleQuantile().domain(data).range(selectedColorRamp);
@@ -95,9 +102,9 @@ function getQuantileScale() {
  * (i.e. equal range of values for each color)
  */
 
-function getEqualIntervalScale() {
+function getEqualIntervalScale(jsonSource) {
   // get density property from each state info entry
-  const data = sourceInfo.features
+  const data = jsonSource.features
     .map((state) => state.properties.density)
     .sort((a, b) => a - b);
 
@@ -116,7 +123,6 @@ function getEqualIntervalScale() {
   // for each density break, get the color
   const colorBreaks = groups.map((d) => color(d));
 
-  console.log(d3.extent(data), colorBreaks);
   // combine density breaks and color breaks into an array of objects
   const colorScale = groups
     .map((d, i) => {
@@ -130,8 +136,8 @@ function getEqualIntervalScale() {
 }
 
 // create function for qualitative data
-function getQualitativeScale() {
-  const data = sourceInfo.features.map((state) => state.properties.race);
+function getQualitativeScale(jsonSource) {
+  const data = jsonSource.features.map((state) => state.properties.race);
 
   // get the total number of unique responses
   const uniqueResponses = [...new Set(data)];
@@ -157,14 +163,36 @@ function getQualitativeScale() {
   return [colorScale, uniqueResponses, colorBreaks];
 }
 
+//   create a function to round the number to a significant digit
+function round(value) {
+  if (value < 1) {
+    return value.toFixed(2);
+  }
+  if (value < 10) {
+    return Math.round(value * 10) / 10;
+  }
+  if (value < 100) {
+    return Math.round(value);
+  }
+  if (value < 1000) {
+    return Math.round(value / 10) * 10;
+  }
+  if (value >= 1000) {
+    return Math.round(value / 1000) + "k";
+  }
+}
+
 mapboxgl.accessToken =
   "pk.eyJ1Ijoibmlrby1kZWxsaWMiLCJhIjoiY2w5c3p5bGx1MDh2eTNvcnVhdG0wYWxkMCJ9.4uQZqVYvQ51iZ64yG8oong";
 const map = new mapboxgl.Map({
   container: "map", // Container ID
   style: "mapbox://styles/niko-dellic/cl9t226as000x14pr1hgle9az", // Map style to use
-  center: [-94.09561, 40.3638], // Starting position [lng, lat]
-  zoom: 4, // Starting zoom level
+  //   center: [-94.09561, 40.3638], // Starting position [lng, lat]
+  //   zoom: 4, // Starting zoom level
   projection: "globe",
+  //   center on new york city
+  center: [-74.006, 40.7128],
+  zoom: 10,
 });
 
 // stylize the globe effect
@@ -180,59 +208,94 @@ map.on("style.load", () => {
 });
 
 map.on("load", () => {
-  // Add a source for the GeoJSON data.
-  map.addSource("states", {
+  map.addSource("nycData", {
     type: "geojson",
-    data: "./data/stateData.geojson",
+    data: "./data/community_boards.json",
   });
 
-  // Add a layer to visualize the state polygons.
   map.addLayer({
-    id: "statedata",
+    id: "nycPoverty",
     type: "fill",
-    source: "states", // reference the data source
+    source: "nycData", // reference the data source
     layout: {},
     paint: {
-      //   style the layer based on the density property
+      // style the layer based on the poverty property
       "fill-color": [
-        "interpolate",
-        ["linear"],
-        ["get", "density"],
-        ...selectedScale[0],
-        // ...getLegend("states"),
+        // add case for if the boundary is not part of the analysis unit
+        "case",
+        ["==", ["get", "Data_YN"], "N"],
+        "rgba(0,0,0,0)",
+        [
+          "interpolate",
+          ["linear"],
+          ["get", "F12_PvtyPc"],
+          ...getQuantileScale(nycData, "F12_PvtyPc")[0],
+        ],
       ],
-
+      //   change opacity when hovering
       "fill-opacity": [
         "case",
         ["boolean", ["feature-state", "hover"], false],
-        1,
-        1,
+        0.8,
+        0.5,
       ],
     },
   });
 
-  //   create a function to round the number to a significant digit
-  function round(value) {
-    if (value < 1) {
-      return value.toFixed(2);
-    }
-    if (value < 10) {
-      return Math.round(value * 10) / 10;
-    }
-    if (value < 100) {
-      return Math.round(value);
-    }
-    if (value < 1000) {
-      return Math.round(value / 10) * 10;
-    }
-    if (value >= 1000) {
-      return Math.round(value / 1000) + "k";
-    }
-  }
+  map.addSource("ethnicity", {
+    type: "geojson",
+    data: "./data/ethnicity.json",
+  });
 
+  // Add a layer to visualize the state polygons.
+  map.addLayer({
+    id: "nycEthnicity",
+    type: "circle",
+    source: "ethnicity", // reference the data source
+    layout: {},
+    paint: {
+      //   make the circle smaller
+      "circle-radius": 2,
+      "circle-pitch-scale": "viewport",
+      // color the circle based on getQualitativeScale
+      "circle-color": [
+        "interpolate",
+        ["linear"],
+        ["get", "EthnicityCode"],
+        1, // hispanic
+        "rgb(244, 133, 0)",
+        2, // white
+        "rgb(29, 168, 39)",
+        3, // black
+        "rgb(80, 128, 234)",
+        4, // indigenous
+        "rgb(128, 63, 138)",
+        5, // asian
+        "rgb(252, 75, 56)",
+        6, // other
+        "rgb(128, 63, 138)",
+      ],
+
+      //   change opacity based on zoom level
+      "circle-opacity": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        10,
+        0,
+        11,
+        0.5,
+        13,
+        1,
+      ],
+    },
+  });
   // create legend
   const legend = document.getElementById("legend");
-  const [legendValues, legendColors] = [selectedScale[1], selectedScale[2]];
+  const [legendValues, legendColors] = [
+    getQuantileScale(nycData, "F12_PvtyPc")[1],
+    getQuantileScale(nycData, "F12_PvtyPc")[2],
+  ];
   legendValues.forEach((layer, i) => {
     const color = legendColors[i];
     const item = document.createElement("div");
@@ -248,11 +311,16 @@ map.on("load", () => {
   });
 
   map.on("mousemove", (event) => {
-    const states = map.queryRenderedFeatures(event.point, {
-      layers: ["statedata"],
+    const bounds = map.queryRenderedFeatures(event.point, {
+      layers: ["nycPoverty"],
     });
-    document.getElementById("pd").innerHTML = states.length
-      ? `<h3>${states[0].properties.name}</h3><p><strong><em>${states[0].properties.density}</strong> people per square mile</em></p>`
-      : `<p>Hover over a state!</p>`;
+
+    if (bounds.length > 0 && bounds[0].properties.Data_YN === "Y") {
+      document.getElementById("pd").innerHTML = bounds.length
+        ? `<h3>${bounds[0].properties.CDTAName}</h3><p><strong><em>${round(
+            bounds[0].properties.F12_PvtyPc
+          )}%</strong> of people are living below the povert line.</em></p>`
+        : `<p>Hover over a community board!</p>`;
+    }
   });
 });
